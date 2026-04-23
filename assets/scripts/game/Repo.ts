e485880feb959d82b022394501b5e1b08045f840
KaiXuanXuan <1749043188@qq.com>
2026-04-23 10:05:22 +0800

    Update Repo.ts

--- assets/scripts/game/Repo.ts
+++ assets/scripts/game/Repo.ts
@@ -11,6 +11,13 @@ export class Repo {
     this.levels.set(1, this.createLevel1());
     this.levels.set(2, this.createLevel2());
     this.levels.set(3, this.createLevel3());
+    this.levels.set(4, this.createLevel4());
+    this.levels.set(5, this.createLevel5());
+    this.levels.set(6, this.createLevel6());
+    this.levels.set(7, this.createLevel7());
+    this.levels.set(8, this.createLevel8());
+    this.levels.set(9, this.createLevel9());
+    this.levels.set(10, this.createLevel10());
   }
 
   static get(id: number): LevelConfig {
@@ -194,6 +201,83 @@ export class Repo {
           "linkedRingId": "ring4"
         }
       ],
+      "rocks": [],
+      "bombs": []
+    };
+  }
+
+  private static createLevel4(): LevelConfig {
+    return {
+      "id": 4,
+      "name": "Level 4",
+      "rings": [],
+      "buckles": [],
+      "rocks": [],
+      "bombs": []
+    };
+  }
+
+  private static createLevel5(): LevelConfig {
+    return {
+      "id": 5,
+      "name": "Level 5",
+      "rings": [],
+      "buckles": [],
+      "rocks": [],
+      "bombs": []
+    };
+  }
+
+  private static createLevel6(): LevelConfig {
+    return {
+      "id": 6,
+      "name": "Level 6",
+      "rings": [],
+      "buckles": [],
+      "rocks": [],
+      "bombs": []
+    };
+  }
+
+  private static createLevel7(): LevelConfig {
+    return {
+      "id": 7,
+      "name": "Level 7",
+      "rings": [],
+      "buckles": [],
+      "rocks": [],
+      "bombs": []
+    };
+  }
+
+  private static createLevel8(): LevelConfig {
+    return {
+      "id": 8,
+      "name": "Level 8",
+      "rings": [],
+      "buckles": [],
+      "rocks": [],
+      "bombs": []
+    };
+  }
+
+  private static createLevel9(): LevelConfig {
+    return {
+      "id": 9,
+      "name": "Level 9",
+      "rings": [],
+      "buckles": [],
+      "rocks": [],
+      "bombs": []
+    };
+  }
+
+  private static createLevel10(): LevelConfig {
+    return {
+      "id": 10,
+      "name": "Level 10",
+      "rings": [],
+      "buckles": [],
       "rocks": [],
       "bombs": []
     };
